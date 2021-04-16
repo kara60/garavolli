e65c2b8ad3b2a4b5be6541cadccffdafe4fd7c8c
Elyas575 <Elyas575@hotmail.com>
2021-04-16 11:05:44 +0300

    comments

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const errorController = require('./controllers/errors');
 
 const User = require('./models/user');
 const ConnectionString = 'mongodb+srv://garavolli:enekcanel@cluster0.chjjh.mongodb.net/node-app?retryWrites=true&w=majority';
-
+ 
 var store = new mongoDbStore({
     uri: ConnectionString,
     collection: 'mySessions'
