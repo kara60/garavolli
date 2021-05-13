daf3ed646f7e3c569425d8028895056161e9bdc8
Enes Kara <secop60@gmail.com>
2021-05-13 14:14:43 +0300

    update fot footer - register - login - navbar

--- public/js/register.js
+++ public/js/register.js
@@ -1,5 +1,17 @@
+const registerCheck = document.querySelector('#registerCheck');
+const registerButton = document.querySelector('#registerButton');
+
+registerCheck.addEventListener('click', () => {
+    if(!registerCheck.checked){
+        registerButton.disabled = true;
+    }else{
+        registerButton.disabled = false;
+    }
+});
+
 function JSalert(){
  
 swal("Congrats!", ", Your account is created!", "success");
  
 }
+
